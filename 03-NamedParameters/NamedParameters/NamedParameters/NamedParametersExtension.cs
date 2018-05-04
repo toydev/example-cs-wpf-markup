6fd0614410a99f7b97da92fce5b0399abe05cdb0
toy <toy@ownway.info>
2018-05-05 00:34:47 +0900

    名前付きパラメータの例を追加した。

---
+++ 03-NamedParameters/NamedParameters/NamedParameters/NamedParametersExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace NamedParameters
+{
+    public class NamedParametersExtension : MarkupExtension
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (Value1 + Value2).ToString();
+        }
+    }
+}
