aa5e51627695b9db0f3eacd0fe2526107cfa189d
toy <toy@ownway.info>
2018-05-05 00:08:34 +0900

    初期登録。

---
+++ 01-HelloWorld/HelloWorld/HelloWorld/HelloWorldExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace HelloWorld
+{
+    public class HelloWorldExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return "Hello Markup World.";
+        }
+    }
+}
