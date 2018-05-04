e5290d34726b6c8b5d15c2f5437f1ab82023e1e7
toy <toy@ownway.info>
2018-05-05 00:14:44 +0900

    引数のサンプルを追加した。

---
+++ 02-Arguments/Arguments/Arguments/ArgumentsExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Arguments
+{
+    public class ArgumentsExtension : MarkupExtension
+    {
+        private string m_name;
+
+        public ArgumentsExtension(string name)
+        {
+            m_name = name;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return string.Format("Hello {0}", m_name);
+        }
+    }
+}
